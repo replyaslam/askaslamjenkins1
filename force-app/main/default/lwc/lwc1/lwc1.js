4258bf37e9cfb71085505ccc8f45fb79f73988c2
replyaslam <replyaslam@gmail.com>
2020-06-25 12:20:33 +0100

    test3

--- force-app/main/default/lwc/lwc1/lwc1.js
+++ force-app/main/default/lwc/lwc1/lwc1.js
@@ -1,6 +1,6 @@
 import { LightningElement } from 'lwc';
 export default class HelloWorld extends LightningElement {
-  //test1,test2
+  //test1,test2,test3
   greeting = 'World';
   changeHandler(event) {
     this.greeting = event.target.value;
